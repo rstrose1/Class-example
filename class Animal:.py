b1302a79529f6fc1bdd2a1e49e78b114b1e2d7d3
rstrose1 <rstrose1@yahoo.com>
2025-02-10 21:28:07 +0000

    initial commit for test

---
+++ class Animal:.py
@@ -0,0 +1,24 @@
+class Animal:
+    def __init__(self, name):
+        self.name = name
+
+    def speak(self):
+        return "Generic animal sound"
+
+class Dog(Animal):
+    def speak(self):
+        return "Woof!"
+
+class Cat(Animal):
+    def speak(self):
+        return "Meow!"
+
+# Instances of the classes
+animal = Animal("Generic Animal")
+dog = Dog("Buddy")
+cat = Cat("Whiskers")
+
+# Calling the speak method for each instance
+print(animal.speak())
+print(dog.speak())
+print(cat.speak())
